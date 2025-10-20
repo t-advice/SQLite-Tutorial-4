8b70f6e6dce948f99c5a01281d7c750566ffc4d5
Tashwill Advice <tashwilladvice02@gmail.com>
2025-10-20 14:00:37 +0200

    app initialisation completed with zero errors for DatabaseService

--- SQLiteTutorial4/App.xaml.cs
+++ SQLiteTutorial4/App.xaml.cs
@@ -1,15 +1,30 @@
-﻿namespace SQLiteTutorial4
+﻿using SQLiteTutorial4.Services;
+
+namespace SQLiteTutorial4
 {
     public partial class App : Application
     {
+        static DatabaseService _database;
+        public static DatabaseService Database
+        {
+            get
+            {
+                if(_database == null)
+                {
+                    string dbPath = Path.Combine(
+                        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                        "Images.db3");
+                    _database = new DatabaseService(dbPath);
+                }
+                return _database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
         }
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
-        }
+        
     }
 }
