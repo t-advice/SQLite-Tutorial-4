b3a6745997f5eb37d800f417e1cea889a78a6d20
Tashwill Advice <tashwilladvice02@gmail.com>
2025-10-20 13:56:18 +0200

    DatabaseService  created for SQLite CRUD

--- SQLiteTutorial4/Models/ImageItem.cs
+++ SQLiteTutorial4/Models/ImageItem.cs
@@ -1,10 +1,10 @@
-﻿using AuthenticationServices;
-using SQLite;
+﻿using SQLite;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace SQLiteTutorial4.Models
 {
---
+++ SQLiteTutorial4/Services/DatabaseService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using SQLiteTutorial4.Models;
+
+namespace SQLiteTutorial4.Services
+{
+    public class DatabaseService
+    {
+        private readonly SQLiteAsyncConnection _database;
+        public DatabaseService(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<ImageItem>().Wait();
+        }
+        // CRUD Operations // All Images , first 
+        
+        public Task<List<ImageItem>> GetImagesAsync()
+        {
+            return _database.Table<ImageItem>()
+                            .OrderByDescending(i => i.CreatedAt)
+                            .ToListAsync();
+        }
+        public Task<ImageItem> GetImageByIdAsync(int id)
+        {
+            return _database.Table<ImageItem>()
+                            .Where(i => i.Id == id)
+                            .FirstOrDefaultAsync();
+        }
+        public Task<int> SaveImageAsync(ImageItem item)
+        {
+            return _database.InsertOrReplaceAsync(item);
+        }
+
+        public Task<int> DeleteImageAsync(ImageItem item)
+        {
+            return _database.DeleteAsync(item);
+        }
+
+    }
+}
