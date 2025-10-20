04f9450d754fd4ac95fd572bc864860a19c121b1
Tashwill Advice <tashwilladvice02@gmail.com>
2025-10-20 13:47:16 +0200

    ImageItem model (ID, FilePath, CreatedAt)

---
+++ SQLiteTutorial4/Models/ImageItem.cs
@@ -0,0 +1,21 @@
+﻿using AuthenticationServices;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteTutorial4.Models
+{
+    public class ImageItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        // File path where the image is stored
+        public string FilePath { get; set; }
+        public string Caption { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
